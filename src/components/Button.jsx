9a84bb79f8e9a9cdd7a765647dcfaa48324ee802
Oyawe <oyawegoodness@gmail.com>
2023-09-12 14:42:13 -0700

    card-deal section complete

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -3,7 +3,8 @@ const Button = ({ styles }) => {
   return (
     <button
       type="button"
-      className={`py-4 px-6 bg-blue-gradient font-poppins font-medium text-[18px] text-primary outline-none ${styles}`}
+      className={`py-4 px-6 bg-blue-gradient font-poppins font-medium text-[18px] text-primary outline-none rounded-md
+      ${styles}`}
     >
       Get Started
     </button>
--- src/components/CardDeal.jsx
+++ src/components/CardDeal.jsx
@@ -1,5 +1,27 @@
-const CardDeal = () => {
-  return <div>CardDeal</div>;
-};
+import styles, { layout } from "../style";
+import { card } from "../assets";
+import Button from "./Button";
+
+const CardDeal = () => (
+  <section className={`${layout.section}`}>
+    <div className={layout.sectionInfo}>
+      <h2 className={styles.heading2}>
+        Find a better card deal <br className="sm:block hidden" /> in few easy
+        steps.
+      </h2>
+      <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sed maxime
+        ducimus sint, cupiditate accusamus assumenda dolores possimus quasi!
+      </p>
+      <Button styles="mt-10" />
+    </div>
+
+    <div className={layout.sectionImg}>
+      <img src={card} alt="card" className="w-[100%] h-[100%] relative z-[5]" />
+      <div className="absolute z-[3] -right-1/2 top-0 w-[50%] h-[50%] rounded-full white__gradient"></div>
+      <div className="absolute z-[0] -right-1/2 bottom-0 w-[50%] h-[50%] rounded-full pink__gradient"></div>
+    </div>
+  </section>
+);
 
 export default CardDeal;
