897ed68a327795eaa48f246c1e20b21fc8494995
Oyawe <oyawegoodness@gmail.com>
2023-09-12 22:14:07 -0700

    error fix

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -20,7 +20,7 @@ const Footer = () => (
       <div className="flex-[1.5] w-full flex flex-row justify-between flex-wrap md:mt-0 mt-10">
         {footerLinks.map((footerLink) => (
           <div
-            key={footerLink.key}
+            key={footerLink.title}
             className="flex flex-col ss:my-0 my-4 min-w-[150px] "
           >
             <h4 className="font-poppins font-medium text-[18px] leading-[27px] text-white">
